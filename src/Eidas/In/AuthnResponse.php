956f096cc2537dd38a12eb8e0413679797835804
Alessandro Pasqualini <alessandro.pasqualini.1105@gmail.com>
2023-09-13 12:44:49 +0200

    Added support for eidas

---
+++ src/Eidas/In/AuthnResponse.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Italia\Spid\Cie\In;
+
+use Italia\Spid\Saml\In\AbstractAuthnResponse;
+use Italia\Spid\Spid\In\AuthnResponse as SpidAuthnResponse;
+
+class AuthnResponse extends SpidAuthnResponse
+{
+    //
+}
